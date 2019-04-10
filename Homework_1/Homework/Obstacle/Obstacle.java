b5786222ce14c188d99d34620ac1d6051f9eb0d9
DWLS <wdegtyarev@outlook.com>
2019-04-10 18:56:27 +0300

    Revert "Merge branch 'homework_1' into Homework5"
    This reverts commit 85d598ef0b22b1b97965e68e1ce71e9a73b4cc95, reversing
    changes made to 3dc576ac2c150b5e5f0a2c8b3ba3798f7c3a7988.

--- Homework_1/Homework/Obstacle/Obstacle.java
+++
@@ -1,9 +0,0 @@
-package Lesson1.Homework.Obstacle;
-
-
-import Lesson1.Homework.Competitors.Competitors;
-
-public abstract class Obstacle {
-
-    public abstract void doIt(Competitors competitors);
-}
